a1dfd0bb60951070054e9f753ba584f948500ec4
matth2k <mrh259@cornell.edu>
2025-02-06 12:21:21 -0500

    update run.sh

--- verilog/iscas85/run.sh
+++ verilog/iscas85/run.sh
@@ -2,5 +2,6 @@
 set -eo pipefail
 
 make -f run.mk clean
-make -f run.mk all TOOL=lvv INFO=$(which yosys)
-make -f run.mk all TOOL=lvv-vivado INFO=$(which vivado)
+make -f run.mk all TOOL=lvv INFO=$(which yosys) -j 8
+make -f run.mk clean
+make -f run.mk all TOOL=lvv-vivado INFO=$(which vivado) -j 8
