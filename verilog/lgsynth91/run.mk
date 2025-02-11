735b09fe144ae10af168e8f870a404482ade26c7
matth2k <mrh259@cornell.edu>
2025-02-11 16:36:49 -0500

    change file names

--- verilog/lgsynth91/run.mk
+++ verilog/lgsynth91/run.mk
@@ -1,16 +1,16 @@
 BENCHMARK_NAME=lgsynth91
-BENCHES=9symml_orig.v alu2_orig.v cm138a_orig.v count_orig.v i2_orig.v pm1_orig.v term1_orig.v \
-C1355_orig.v alu4_orig.v cm150a_orig.v cu_orig.v i3_orig.v rot_orig.v too_large_orig.v \
-C1908_orig.v apex7_orig.v cm152a_orig.v decod_orig.v i5_orig.v unreg_orig.v \
-C17_orig.v apex6_orig.v cm151a_orig.v dalu_orig.v i4_orig.v ttt2_orig.v \
-C2670_orig.v b1_orig.v cm162a_orig.v des_orig.v i6_orig.v vda_orig.v \
-C3540_orig.v b9_orig.v cm163a_orig.v i7_orig.v x1_orig.v \
-C432_orig.v cm42a_orig.v example2_orig.v i8_orig.v mux_orig.v x2_orig.v \
-C499_orig.v c8_orig.v cm82a_orig.v f51m_orig.v i9_orig.v my_adder_orig.v s953_orig.v x3_orig.v \
-C5315_orig.v cc_orig.v cm85a_orig.v frg1_orig.v k2_orig.v pair_orig.v x4_orig.v \
-cht_orig.v cmb_orig.v frg2_orig.v lal_orig.v parity_orig.v sct_orig.v z4ml_orig.v \
-C7552_orig.v comp_orig.v i10_orig.v majority_orig.v pcle_orig.v t481_orig.v C880_orig.v \
-cordic_orig.v i1_orig.v pcler8_orig.v tcon_orig.v
+BENCHES=9symml.v alu2.v cm138a.v count.v i2.v pm1.v term1.v \
+C1355.v alu4.v cm150a.v cu.v i3.v rot.v too_large.v \
+C1908.v apex7.v cm152a.v decod.v i5.v unreg.v \
+C17.v apex6.v cm151a.v dalu.v i4.v ttt2.v \
+C2670.v b1.v cm162a.v des.v i6.v vda.v \
+C3540.v b9.v cm163a.v i7.v x1.v \
+C432.v cm42a.v example2.v i8.v mux.v x2.v \
+C499.v c8.v cm82a.v f51m.v i9.v my_adder.v s953.v x3.v \
+C5315.v cc.v cm85a.v frg1.v k2.v pair.v x4.v \
+cht.v cmb.v frg2.v lal.v parity.v sct.v z4ml.v \
+C7552.v comp.v i10.v majority.v pcle.v t481.v C880.v \
+cordic.v i1.v pcler8.v tcon.v
 RPTS=$(BENCHES:.v=.v.rpt)
 
 TOOL=lvv
