9818dc7da845dd2a3a3f7c020b21fcef9d509a0c
matth2k <mrh259@cornell.edu>
2025-02-11 19:13:25 -0500

    use 16 cpus

--- verilog/iscas85/run.sh
+++ verilog/iscas85/run.sh
@@ -4,22 +4,22 @@ set -eo pipefail
 make -f run.mk clean_lite
 
 source /work/shared/common/yosys/33/setup.sh
-make -f run.mk iscas85_lvv_results.json TOOL=lvv INFO=yosys33, -j 8 TIMEOUT=600
+make -f run.mk iscas85_lvv_results.json TOOL=lvv INFO=yosys33, -j 16 TIMEOUT=600
 mv iscas85_lvv_results.json yosys_no_decomp.json
 rm -rf *.rpt
 
 source /opt/xilinx/Vivado/2022.1/settings64.sh
-make -f run.mk all TOOL=lvv-vivado INFO=v2022, -j 10  TIMEOUT=600
+make -f run.mk all TOOL=lvv-vivado INFO=v2022, -j 16  TIMEOUT=600
 mv iscas85_lvv-vivado_results.json vivado_no_decomp.json
 rm -rf *.rpt
 
 source /work/shared/common/yosys/33/setup.sh
-make -f run.mk all TOOL=lvv INFO=yosys33,dyn-decomp -j 10  TIMEOUT=3600 EXTRA_FLAGS="--decomp"
+make -f run.mk all TOOL=lvv INFO=yosys33,dyn-decomp -j 16  TIMEOUT=3600 EXTRA_FLAGS="--decomp"
 mv iscas85_lvv_results.json yosys_decomp.json
 rm -rf *.rpt
 
 source /opt/xilinx/Vivado/2022.1/settings64.sh
-make -f run.mk all TOOL=lvv-vivado INFO=v2022,dyn-decomp -j 10  TIMEOUT=3600 EXTRA_FLAGS="--decomp"
+make -f run.mk all TOOL=lvv-vivado INFO=v2022,dyn-decomp -j 16  TIMEOUT=3600 EXTRA_FLAGS="--decomp"
 mv iscas85_lvv-vivado_results.json vivado_decomp.json
 rm -rf *.rpt
 
