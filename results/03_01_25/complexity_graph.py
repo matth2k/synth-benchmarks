cc1e48ba60f793640363ade7def0694035f2eedb
berkgokmen14 <berkgokmen2002@outlook.com>
2025-03-01 17:48:31 -0400

    Added complexity graph

---
+++ results/03_01_25/complexity_graph.py
@@ -0,0 +1,32 @@
+import json
+import matplotlib.pyplot as plt
+
+def get_iter(name):
+    return int(name[name.find("_") + 1:])
+
+if __name__ == "__main__":
+    with open('yosys33_iter_data.json', 'r') as file:
+        data = json.load(file)
+
+    plt.figure(figsize=(12, 8))
+    plt.title('Build Time vs. Number of Iterations')
+
+    iteration_dict = {}
+
+    for module in data["modules"]:
+        for iteration_data in data["modules"][module]:
+            iteration = get_iter(iteration_data)
+            build_time = data["modules"][module][iteration_data]["build_time"]
+            
+            if iteration not in iteration_dict:
+                iteration_dict[iteration] = []
+            iteration_dict[iteration].append(build_time)
+
+    iterations = sorted(iteration_dict.keys())
+    avg_build_times = [sum(times) / len(times) for times in [iteration_dict[iter] for iter in iterations]]
+
+    plt.plot(iterations, avg_build_times, marker='o')
+    plt.xlabel("Number of Iterations")
+    plt.ylabel("Average Build Time (s)")
+    plt.tight_layout()
+    plt.savefig('./complexity_graph.png', bbox_inches='tight')
