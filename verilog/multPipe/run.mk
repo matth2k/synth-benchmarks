4365a67967f567298b478c1bb28f6ca4b34435f9
matth2k <mrh259@cornell.edu>
2025-03-05 16:00:00 -0500

    add files for multPipe

---
+++ verilog/multPipe/run.mk
@@ -0,0 +1,28 @@
+BENCHMARK_NAME=multPipe
+BENCHES=multPipe_n1.v multPipe_n2.v multPipe_n4.v multPipe_n8.v multPipe_n16.v
+
+RPTS=$(BENCHES:.v=.v.rpt)
+
+TOOL=lvv
+INFO=$(which yosys)
+CAT_TOOL=cat.py
+
+TIMEOUT=1
+EXTRA_FLAGS=
+FLAGS+=-t $(TIMEOUT) -s 10000000 -n 10000000 --no-verify $(EXTRA_FLAGS) 
+
+.PHONY: all clean clean_lite
+
+all: $(BENCHMARK_NAME)_$(TOOL)_results.json
+
+clean: 
+	rm -rf *.v.rpt *.tcl *.ys *.json
+
+clean_lite: 
+	rm -rf *.v.rpt *.tcl *.ys
+
+$(BENCHMARK_NAME)_$(TOOL)_results.json: $(RPTS)
+	$(CAT_TOOL) --version $(INFO) $(RPTS) > $@
+
+%.v.rpt: %.v
+	+$(TOOL) $< $(FLAGS) --report $@ >> /dev/null
---
+++ verilog/multPipe/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -eo pipefail
+
+make -f run.mk clean_lite
+
+source /work/shared/common/yosys/33/setup.sh
+make -f run.mk multPipe_lvv_results.json TOOL=lvv INFO=yosys33, -j 16 TIMEOUT=600
+mv multPipe_lvv_results.json yosys_no_decomp.json
+rm -rf *.rpt
+
+source /opt/xilinx/Vivado/2022.1/settings64.sh
+make -f run.mk all TOOL=lvv-vivado INFO=v2022, -j 16  TIMEOUT=600
+mv multPipe_lvv-vivado_results.json vivado_no_decomp.json
+rm -rf *.rpt
+
+source /work/shared/common/yosys/33/setup.sh
+make -f run.mk all TOOL=lvv INFO=yosys33,dyn-decomp -j 16  TIMEOUT=3600 EXTRA_FLAGS="--decomp"
+mv multPipe_lvv_results.json yosys_decomp.json
+rm -rf *.rpt
+
+source /opt/xilinx/Vivado/2022.1/settings64.sh
+make -f run.mk all TOOL=lvv-vivado INFO=v2022,dyn-decomp -j 16  TIMEOUT=3600 EXTRA_FLAGS="--decomp"
+mv multPipe_lvv-vivado_results.json vivado_decomp.json
+rm -rf *.rpt
+
+python3 ../../utils/merge.py yosys_no_decomp.json vivado_no_decomp.json yosys_decomp.json vivado_decomp.json > multPipe_master.json
