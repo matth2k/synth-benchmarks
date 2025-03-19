bfd8be8af837c15512faa030bbd60c408c819015
berkgokmen14 <berkgokmen2002@outlook.com>
2025-03-18 21:55:21 -0300

    Added script for collecting iteration data

---
+++ verilog/run_iter_data.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -eo pipefail
+
+source /work/shared/common/yosys/33/setup.sh
+mkdir -p yosys33-iter-data
+
+for i in {1..100}; do
+    cd iscas85
+    make -f run.mk iscas85_lvv_results.json TOOL=lvv INFO=yosys33_${i}, -j 16 TIMEOUT=600 ITERATION_LIMIT=${i}
+    mv iscas85_lvv_results.json iscas85_${i}.json
+    rm -rf *.rpt
+    cp iscas85_${i}.json ../yosys33-iter-data
+
+    cd ../lgsynth91
+    make -f run.mk lgsynth91_lvv_results.json TOOL=lvv INFO=yosys33_${i}, -j 16 TIMEOUT=600 ITERATION_LIMIT=${i}
+    mv lgsynth91_lvv_results.json lgsynth91_${i}.json
+    rm -rf *.rpt
+    cp lgsynth91_${i}.json ../yosys33-iter-data
+
+    cd ../epfl
+    make -f run.mk epfl_lvv_results.json TOOL=lvv INFO=yosys33_${i}, -j 16 TIMEOUT=600 ITERATION_LIMIT=${i}
+    mv epfl_lvv_results.json epfl_${i}.json
+    rm -rf *.rpt
+    cp epfl_${i}.json ../yosys33-iter-data
+
+    cd ..
+done
+
+cd /yosys33-iter-data
+json_files=$(ls *.json) && python3 ../../utils/merge.py $json_files > yosys33_iter_data.json
+cd ..
