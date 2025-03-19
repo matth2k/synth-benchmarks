3fe9ea2f85a15692d872e36280cc340134b8cba6
berkgokmen14 <berkgokmen2002@outlook.com>
2025-03-18 21:56:47 -0300

    Updated alu data collection script

--- verilog/alu/run.sh
+++ verilog/alu/run.sh
@@ -3,23 +3,13 @@ set -eo pipefail
 
 make -f run.mk clean_lite
 
-values="1 30 60 300 600 1800 3600"
+values="300"
 
 for value in $values; do
-
-    source /opt/xilinx/Vivado/2019.2/settings64.sh
-    make -f run.mk all TOOL=lvv-vivado INFO=v2019-t-${value} -j 10  TIMEOUT=${value}
-    mv alus_lvv-vivado_results.json vivado2019_no_decomp_t_${value}.json
-    rm -rf *.rpt
 
     source /opt/xilinx/Vivado/2022.1/settings64.sh
     make -f run.mk all TOOL=lvv-vivado INFO=v2022-t-${value} -j 10  TIMEOUT=${value}
     mv alus_lvv-vivado_results.json vivado2022_no_decomp_t_${value}.json
-    rm -rf *.rpt
-
-    source /work/shared/common/yosys/47/setup.sh 
-    make -f run.mk alus_lvv_results.json TOOL=lvv INFO=yosys47-t-${value} -j 10 TIMEOUT=${value}
-    mv alus_lvv_results.json yosys47_no_decomp_t_${value}.json
     rm -rf *.rpt
 
     source /work/shared/common/yosys/33/setup.sh 
@@ -27,19 +17,9 @@ for value in $values; do
     mv alus_lvv_results.json yosys33_no_decomp_t_${value}.json
     rm -rf *.rpt
 
-    source /opt/xilinx/Vivado/2019.2/settings64.sh
-    make -f run.mk all TOOL=lvv-vivado INFO=v2019-decomp-t-${value} -j 10  TIMEOUT=${value} EXTRA_FLAGS="--decomp"
-    mv alus_lvv-vivado_results.json vivado2019_decomp_t_${value}.json
-    rm -rf *.rpt
-
     source /opt/xilinx/Vivado/2022.1/settings64.sh
     make -f run.mk all TOOL=lvv-vivado INFO=v2022-decomp-t-${value} -j 10  TIMEOUT=${value} EXTRA_FLAGS="--decomp"
     mv alus_lvv-vivado_results.json vivado2022_decomp_t_${value}.json
-    rm -rf *.rpt
-
-    source /work/shared/common/yosys/47/setup.sh 
-    make -f run.mk alus_lvv_results.json TOOL=lvv INFO=yosys47-decomp-t-${value} -j 10 TIMEOUT=${value} EXTRA_FLAGS="--decomp"
-    mv alus_lvv_results.json yosys47_decomp_t_${value}.json
     rm -rf *.rpt
 
     source /work/shared/common/yosys/33/setup.sh 
