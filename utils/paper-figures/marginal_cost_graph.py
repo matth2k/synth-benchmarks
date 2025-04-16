5aed57b37460cf88cf12a0269e74f546d6bc5282
matth2k <mrh259@cornell.edu>
2025-04-16 17:49:17 -0400

    E-graph -> E-Graph

--- utils/paper-figures/marginal_cost_graph.py
+++ utils/paper-figures/marginal_cost_graph.py
@@ -5,15 +5,19 @@ import json
 import matplotlib.pyplot as plt
 import sys
 
+
 def get_iter(name):
-    return int(name[name.find("_") + 1:])
+    return int(name[name.find("_") + 1 :])
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Given a JSON file with iteration data, produces the runtime and marginal cost graphs"
     )
     parser.add_argument(
-        "input", type=argparse.FileType("r"), help="Path to the input JSON file with iteration data"
+        "input",
+        type=argparse.FileType("r"),
+        help="Path to the input JSON file with iteration data",
     )
     args = parser.parse_args()
 
@@ -27,7 +31,7 @@ if __name__ == "__main__":
             iteration = get_iter(iteration_data)
             build_time = data["modules"][module][iteration_data]["build_time"]
             saturated = data["modules"][module][iteration_data]["saturated"]
-            
+
             # Don't include saturated build time
             if saturated:
                 break
@@ -40,11 +44,10 @@ if __name__ == "__main__":
     # Calculate averages and derivatives
     iterations = sorted(iteration_dict.keys())
     avg_build_times = [
-        sum(times) / len(times) for times in [iteration_dict[iter] for iter in iterations]
+        sum(times) / len(times)
+        for times in [iteration_dict[iter] for iter in iterations]
     ]
-    derivative_build_times = [
-        0
-    ] + [
+    derivative_build_times = [0] + [
         avg_build_times[i] - avg_build_times[i - 1]
         for i in range(1, len(avg_build_times))
     ]
@@ -57,14 +60,22 @@ if __name__ == "__main__":
 
     # Plot 1: Marginal Cost of Iteration
     plt.figure(figsize=(12, 8))
-    plt.title("Marginal Cost of E-graph Rewrite Iteration", fontsize=22)
-    plt.plot(iterations[:max_iteration], derivative_build_times[:max_iteration], linewidth=2.5)
+    plt.title("Marginal Cost of E-Graph Rewrite Iteration", fontsize=22)
+    plt.plot(
+        iterations[:max_iteration],
+        derivative_build_times[:max_iteration],
+        linewidth=2.5,
+    )
     plt.xlabel("Rewrite Iteration", fontsize=22)
     plt.ylabel("Iteration Execution Time (s)", fontsize=22)
     plt.tight_layout()
     plt.xticks(fontsize=19)
     plt.yticks(fontsize=19)
-    plt.savefig(os.path.join(graphs_dir, "runtime_derivative.pdf"), format="pdf", bbox_inches="tight")
+    plt.savefig(
+        os.path.join(graphs_dir, "runtime_derivative.pdf"),
+        format="pdf",
+        bbox_inches="tight",
+    )
 
     # Plot 2: Build Time vs. Iterations
     plt.figure(figsize=(12, 8))
@@ -75,4 +86,6 @@ if __name__ == "__main__":
     plt.tight_layout()
     plt.xticks(fontsize=19)
     plt.yticks(fontsize=19)
-    plt.savefig(os.path.join(graphs_dir, "runtime.pdf"), format="pdf", bbox_inches="tight")
+    plt.savefig(
+        os.path.join(graphs_dir, "runtime.pdf"), format="pdf", bbox_inches="tight"
+    )
